3ed5ad224782575ed24be3925f990857fb010e56
zweiyi <2980086579@qq.com>
2019-10-11 14:54:14 +0800

    第三次提交

--- Calculator/Controller/ViewController.m
+++ Calculator/Controller/ViewController.m
@@ -10,7 +10,10 @@
 #import "Masonry.h"
 #import "CalculatorModel.h"
 
-@interface ViewController ()
+@interface ViewController () {
+    int flagpoint;
+    int flagstr;
+}
 
 @end
 
@@ -41,13 +44,17 @@
         [button addTarget:self action:@selector(clickButton:) forControlEvents:UIControlEventTouchUpInside];
         
     }
+    flagpoint = 0;
+    flagstr = 0;
 }
 
 -(void)clickButton:(UIButton *)button {
 //    double res;
+    
     if (button.tag == 100) {
         self.calview.textView.text = @"";
         [_dataString setString:@""];
+        flagpoint = 0;
     } else if (button.tag == 107) {
 //        NSLog(@"dengyu");
         [_dataString appendString:@"#"];
@@ -64,11 +71,106 @@
 
 //        NSMutableString *str = [NSMutableString stringWithString:_dataString];
 //        [str appendString:@"÷"];
-        [_dataString appendString:@"/"];
+        
+        flagpoint = 0;
+        if (_dataString.length >= 1) {
+            NSString *str = [NSString stringWithString: [_dataString substringFromIndex:_dataString.length - 1]];
+            if (![str isEqualToString:@"/"] && ![str isEqualToString:@"*"] && ![str isEqualToString:@"+"] && ![str isEqualToString:@"-"]) {
+                [_dataString appendString:@"/"];
+            }
+        }
+        
         self.calview.textView.text = _dataString;
-//        [_dataString appendString:@"/"];
     } else {
-        [_dataString appendString:button.titleLabel.text];
+        //点击小数点
+        if ([button.titleLabel.text  isEqual: @"."]) {
+            if ([_dataString isEqual:@""]) {
+                [_dataString setString:@"0."];
+                self.calview.textView.text = _dataString;
+            } else {
+                if (flagpoint == 0) {
+                    NSString *str1 = [NSString stringWithString:[_dataString substringFromIndex:_dataString.length - 1]];
+                    
+                    //防止“+.”等情况出现
+                    if (![str1 isEqualToString:@"/"] && ![str1 isEqualToString:@"*"] && ![str1 isEqualToString:@"+"] && ![str1 isEqualToString:@"-"] && ![str1 isEqualToString:@"("] && ![str1 isEqualToString:@")"]) {
+                        flagpoint = 1;
+                        [_dataString appendString:button.titleLabel.text];
+                    }
+                }
+            }
+            
+        } else if ([button.titleLabel.text isEqual:@"0"] /*&& flagpoint == 0 && _dataString.length >= 1*/) {            //点击零
+            if (_dataString.length >= 1 && flagpoint == 0) {
+                NSString *str = [NSString stringWithString: [_dataString substringFromIndex:_dataString.length - 1]];
+                if (![str isEqualToString:button.titleLabel.text]) {
+                    [_dataString appendString:button.titleLabel.text];
+                }
+            } else {
+                [_dataString appendString:button.titleLabel.text];
+            }
+            
+        } else {                    //点击其余按钮
+            
+            NSString *str = button.titleLabel.text;
+            if ([str isEqual:@"+"] || [str isEqual:@"-"] || [str isEqual:@"*"] || [str isEqual:@"("] || [str isEqual:@")"]) {
+                flagpoint = 0;
+                
+                
+                if ([str isEqualToString:@"("] || [str isEqualToString:@")"]) {
+                    if (_dataString.length >= 1 )  {
+                        if ([str isEqualToString:@")"]) {
+                            NSString *str1 = [NSString stringWithString:[_dataString substringFromIndex:_dataString.length - 1]];
+                            if (![str1 isEqualToString:@"("] &&![str1 isEqualToString:@"/"] && ![str1 isEqualToString:@"*"] && ![str1 isEqualToString:@"+"] && ![str1 isEqualToString:@"-"]
+                                && ![str1 isEqualToString:@"."]) {
+                                [_dataString appendString:button.titleLabel.text];
+                            }
+                        } else {
+                            NSString *str1 = [NSString stringWithString:[_dataString substringFromIndex:_dataString.length - 1]];
+                            if ([str1 isEqualToString:@"("] || [str1 isEqualToString:@"/"] || [str1 isEqualToString:@"*"] || [str1 isEqualToString:@"+"] || [str1 isEqualToString:@"-"]) {
+                                [_dataString appendString:button.titleLabel.text];
+                            }
+                        }
+                        
+                    } else {
+                        if ([str isEqualToString:@"("]) {
+                            [_dataString appendString:button.titleLabel.text];
+                        }
+                    }
+                    
+               } else
+                
+                //负数的第一次输入
+                if([str isEqualToString:@"-"]) {
+                    if (_dataString.length == 0) {
+                        [_dataString appendString:button.titleLabel.text];
+                    } else {
+                        NSString *str1 = [NSString stringWithString:[_dataString substringFromIndex:_dataString.length - 1]];
+                        if (![str1 isEqualToString:@"/"] && ![str1 isEqualToString:@"*"] && ![str1 isEqualToString:@"+"] && ![str1 isEqualToString:@"-"]) {
+                            [_dataString appendString:button.titleLabel.text];
+                        }
+                    }
+                } else {
+                    if (_dataString.length >= 1) {
+                        NSString *str1 = [NSString stringWithString:[_dataString substringFromIndex:_dataString.length - 1]];
+                        if (![str1 isEqualToString:@"/"] && ![str1 isEqualToString:@"*"] && ![str1 isEqualToString:@"+"] && ![str1 isEqualToString:@"-"] &&![str1 isEqualToString:@"("] && ![str1 isEqualToString:@")"]) {
+                            [_dataString appendString:button.titleLabel.text];
+                        }
+                    }
+                }
+                
+                //连续运算符的输入判断
+//                if (_dataString.length >= 1) {
+//                    NSString *lastStr = [NSString stringWithString:[_dataString substringFromIndex:_dataString.length - 1]];
+//                    if (![lastStr isEqualToString:@"/"] && ![lastStr isEqualToString:@"*"] && ![lastStr isEqualToString:@"+"] && ![lastStr isEqualToString:@"-"]) {
+//                        [_dataString appendString:button.titleLabel.text];
+//                    }
+//                }
+                
+            } else {
+                [_dataString appendString:button.titleLabel.text];
+            }
+        }
+        
         self.calview.textView.text = _dataString;
     }
 }
--- Calculator/Model/CalculatorModel.m
+++ Calculator/Model/CalculatorModel.m
@@ -37,14 +37,14 @@ int compare (char ch_top, char ch_cur) {
     int cmp[7][7]  = {  { 2, 2, 1, 1, 1, 2, 2 },
                         { 2, 2, 1, 1, 1, 2, 2 },
                         { 2, 2, 2, 2, 1, 2, 2 },
-                        { 2, 2, 2, 2, 1, 1, 2 },
+                        { 2, 2, 2, 2, 1, 2, 2 },
                         { 1, 1, 1, 1, 1, 3, 0 },
                         { 2, 2, 2, 2, 0, 2, 2 },
                         { 1, 1, 1, 1, 1, 0, 3 }  };
     
     char *opset = "+-*/()#";
     int i, m_top, m_cur;
-    NSLog(@"top %c and %c", ch_top, ch_cur);
+//    NSLog(@"top %c and %c", ch_top, ch_cur);
     for(i = 0; i < 7; i++) {
         if(ch_top == opset[i]) {
             m_top = i;
@@ -80,8 +80,7 @@ double PopNum(Num *top) {
     double data;
     p = top->next;
     if(top->next == NULL) {
-        printf("栈空");
-        exit(1);
+        return 0;
     }
     pdel = top->next;
     //printf("num--%d", pdel->num);
@@ -89,6 +88,7 @@ double PopNum(Num *top) {
     top->next = pdel->next;
     free(pdel);
     return data;
+    
 }
 
 char PopOPTR(OPTR *top) {
@@ -151,6 +151,12 @@ int Inopset(char ch) {
     int len;
     int top = 0;
     len = (int)strlen(string);
+    _flagsucc = [NSNumber numberWithInt:1];
+    if (string[0] == '.') {
+        _flagsucc = 0;
+    }
+    int numflag = 0;
+    //括号匹配，2.3.3.3
     for (int i = 0; i < len; i++) {
         ch = string[i];
         if (ch == '(') {
@@ -164,12 +170,20 @@ int Inopset(char ch) {
                 top--;
             }
         }
+        if (!Inopset(ch) && ch != '(' && ch != ')') {
+            numflag = 1;
+        }
+    
     }
     if (top != 0) {
         _flagsucc = 0;
     } else {
         ;
     }
+    if (!numflag) {
+        _flagsucc = 0;
+    }
+    
     for (int i = 0; i < len - 1; i++) {
         if (string[i] == ')' && !Inopset(string[i + 1])) {
             _flagsucc = 0;
@@ -180,11 +194,21 @@ int Inopset(char ch) {
             break;
         }
     }
+    
+    for (int i = 1; i < len - 1; i++) {
+        //+.3
+        if ( Inopset(string[i - 1]) && (string[i] == '.') && !Inopset(string[i + 1]) && (string[i + 1] != '.') ){
+            _flagsucc = 0;
+            break;
+        }
+    }
+    
    
 }
 
 -(void)Exp {
     int len, i = 0;
+    int flag = 0;   //判断是减号还是负号
    const char *mess;
     char ch, opch;
     double data, num1, num2, res;
@@ -197,25 +221,60 @@ int Inopset(char ch) {
     PushOPTR(optr_top, '#');
     ch = mess[i];
     [self IsRight:mess];
+    //如果表达式输入正确
     if (_flagsucc != 0) {
         while (ch != '#' || GetOPTR(optr_top) != '#') {
-            if (!Inopset(ch)) {
+            //如果第一个为负数
+            if (mess[0] == '-') {
+                i++;
+                ch = mess[i];
+                if (!Inopset(ch)) {
+                    //                NSLog(@"ch === %c", ch);
+                    data = ch - '0';
+                    i++;
+                    ch = mess[i];
+                    int flag = 0;       //含小数点
+                    while (!Inopset(ch)) {
+                        //                    NSLog(@"data == %f", data);
+                        if (ch == '.') {
+                            flag = 1;
+                            i++;
+                            ch = mess[i];
+                            continue;
+                        }
+                        if (flag) {
+                            data = data + (ch - '0') * pow(10, -flag);
+                            flag++;
+                            //data = data * 10 + ch - '0';
+                            //                        NSLog(@"1=");
+                        } else {
+                            //flag = 1;
+                            data = data * 10 + ch - '0';
+                        }
+                        i++;
+                        ch = mess[i];
+                        //                    NSLog(@"data == %f", data);
+                    }
+                    data = -data;
+                    PushNum(num_top, data);
+                }
+            } else if (!Inopset(ch)) {
 //                NSLog(@"ch === %c", ch);
                 data = ch - '0';
                 i++;
                 ch = mess[i];
-                int flag = 0;       //含小数点
+                int flagpoint = 0;       //含小数点
                 while (!Inopset(ch)) {
 //                    NSLog(@"data == %f", data);
                     if (ch == '.') {
-                        flag = 1;
+                        flagpoint = 1;
                         i++;
                         ch = mess[i];
                         continue;
                     }
-                    if (flag) {
-                        data = data + (ch - '0') * pow(10, -flag);
-                        flag++;
+                    if (flagpoint) {
+                        data = data + (ch - '0') * pow(10, -flagpoint);
+                        flagpoint++;
                         //data = data * 10 + ch - '0';
 //                        NSLog(@"1=");
                     } else {
@@ -226,8 +285,22 @@ int Inopset(char ch) {
                     ch = mess[i];
 //                    NSLog(@"data == %f", data);
                 }
-                PushNum(num_top, data);
+                
+                if (flag == 1) {
+                    PushNum(num_top, data * (-1));
+                } else {
+                    PushNum(num_top, data);
+                }
             } else {
+                char ch1 = mess[i-1];
+                if (ch1 == '(' && ch == '-') {
+                    flag = 1;
+                    i++;
+                    ch = mess[i];
+                    continue;
+                } else {
+                    flag = 0;
+                }
                 switch (compare(GetOPTR(optr_top), ch)) {
                     case 1:
                         PushOPTR(optr_top, ch);
